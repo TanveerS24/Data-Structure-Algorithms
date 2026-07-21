ebc3903b8d089946d4cb1f420a441812e7ab2495
TanveerS24 <stanveer1809@gmail.com>
2026-07-21 19:14:44 +0530

    Progress

--- ds/app/BinarySearchTree.java
+++ ds/app/BinarySearchTree.java
@@ -1,7 +1,10 @@
 package ds.app;
 
 import ds.tree.Node;
+
 //import java.util.Scanner;
+import java.util.Queue;
+import java.util.ArrayDeque;
 
 public class BinarySearchTree {
     
@@ -29,8 +32,68 @@ public class BinarySearchTree {
         return root;
     }
 
-    public void delete(int data){
-        
+    public Node delete(Node root, int data){
+        if(root == null){
+            return null;
+        }
+
+        if(root.data<data) 
+            root.right = delete(root.right, data);
+        else if(root.data>data)
+            root.left = delete(root.left, data);
+        else{
+            if(root.left == null && root.right ==null)
+                root = null;
+            
+            else if(root.left == null)
+                return root.right;
+            else if(root.right == null)
+                return root.left;
+            else{
+                Node Successor = findMin(root.right);
+                root.data = Successor.data;
+                root.right = delete(root.right, Successor.data);
+            }
+        }
+
+        return root;
+    }
+
+    public Node findMin(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+
+    //public boolean validateBST(){}
+    
+    //public int kthSmallestElement(){}
+
+    //public int kthLargestElement(){}
+
+    //public boolean isBalanced(){}
+
+    //public boolean isBinarySearchTree(){}
+
+    //public Node mirror(){}
+
+    public void breadthFirstSearch(Node root){
+        if(root == null){
+            return;
+        }
+
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            Node current = queue.poll();
+
+			System.out.print(current.data+" ");
+
+			if(current.left!=null) queue.offer(current.left);
+			if(current.right != null) queue.offer(current.right);
+        }
     }
 
     public int findDepth(Node root){
